68343c4ee7315edcd4d55f286bb7eebdcc71e88c
Rafael Poerner <rafapoerner@yahoo.com.br>
2022-06-30 02:43:37 -0300

    Implementação do Database

--- back-end/src/ProAtividade.API/Startup.cs
+++ back-end/src/ProAtividade.API/Startup.cs
@@ -1,16 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using ProAtividade.API.Data;
 
 namespace ProAtividade.API
 {
@@ -27,7 +30,13 @@ namespace ProAtividade.API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddDbContext<DataContext>(
+                options => options.UseSqlite(Configuration.GetConnectionString("Default"))
+            );
+            services.AddControllers()
+                    .AddJsonOptions(options => {
+                        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                    });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProAtividade.API", Version = "v1" });
--- back-end/src/ProAtividade.API/models/Atividade.cs
+++ back-end/src/ProAtividade.API/models/Atividade.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ProAtividade.API.models
+namespace ProAtividade.API.Models
 {
     public class Atividade
     {
         public int Id { get; set; }
         public string Titulo { get; set; }
         public string Descricao { get; set; }
-        public string Prioridade { get; set; }
+        public Prioridade Prioridade { get; set; }
 
-        public Atividade(){ }
+        public Atividade() { }
         public Atividade(int id)
         {
             Id = id;
